0b49f708a3ef2af28fbab1f135009fe7a4b0d482
sbarnes88 <65136741+sbarnes88@users.noreply.github.com>
2020-06-10 12:03:02 -0600

    Initial Commit

---
+++ Models/Document.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWeb.Models
+{
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        public int StatusId { get; set; }
+        
+    }
+}
